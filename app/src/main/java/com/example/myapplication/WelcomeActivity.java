eb352645045154beea71e921854f79feb55eb246
aminamldsnv <aishanuramina@gmail.com>
2025-10-03 09:00:09 -0500

    10/3

--- app/src/main/java/com/example/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/myapplication/WelcomeActivity.java
@@ -55,12 +55,12 @@ public class WelcomeActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+//    @Override
+//    public void onBackPressed() {
+//        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+//            drawerLayout.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
 }
